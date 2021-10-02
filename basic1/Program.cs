a9f47d752cb885c66c589e119e5ab5aba5080f55
AleWaymelAlexandre <103083020@student.swin.edu.au>
2021-10-02 17:57:15 +1000

    ok

---
+++ basic1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace basic1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            klasdlkasd
+        }
+    }
+}
