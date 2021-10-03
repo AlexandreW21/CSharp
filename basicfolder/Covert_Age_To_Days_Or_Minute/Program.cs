1a4b53702d4ae39857504ba07c8ec813576f1250
AleWaymelAlexandre <103083020@student.swin.edu.au>
2021-10-03 15:22:51 +1100

    just realizing how much a forgot

---
+++ basicfolder/Covert_Age_To_Days_Or_Minute/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Covert_Age_To_Days_Or_Minute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearuser = int.Parse(Console.ReadLine());
+            int Monthuser = int.Parse(Console.ReadLine());
+            int dateuser = int.Parse(Console.ReadLine());
+            CovertInday(yearuser , Monthuser, dateuser);
+        }
+
+        static void CovertInday(int AgeUser, int MonthUser, int DayUser)
+        {
+            int YearNow = DateTime.Now.Date.Year;
+            int MonthNow = DateTime.Now.Date.Month;
+            int DayNow = DateTime.Now.Date.Day;
+            // int HourNow = DateTime.Now.Hour;
+            // int MinuteNow = DateTime.Now.Minute;
+            // int SecondNow = DateTime.Now.Second;
+
+            
+            int YearOfB = YearNow - AgeUser;
+            int MonthOfB = MonthNow - (MonthUser - MonthNow); 
+            
+            
+            System.Console.WriteLine((YearOfB * 12) + ( MonthUser - MonthNow)+MonthNow);
+            
+        }
+    }
+}
+
+
+
+/*
+            int Year = DateTime.Now.Date.Year;
+            int Month = DateTime.Now.Date.Month;
+            int Day = DateTime.Now.Date.Day;
+            int Hour = DateTime.Now.Hour;
+            int Minute = DateTime.Now.Minute;
+            int Second = DateTime.Now.Second;
+            System.Console.WriteLine($"{Year} Year {Month} Month {Day} Day {Hour} Hours {Minute} Minuite {Second} Seconde");
+*/
