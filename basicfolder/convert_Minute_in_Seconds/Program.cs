6ed4253d6c8b5a40c67a45f46fededef4d5e72e5
AleWaymelAlexandre <103083020@student.swin.edu.au>
2021-10-03 13:38:51 +1100

    Min In sec

---
+++ basicfolder/convert_Minute_in_Seconds/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace convert_Minute_in_Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a function that takes an integer minutes and converts it to seconds.
+            System.Console.WriteLine("Howmany minut to you want a convert? \n");
+            int inputUser = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine(MinInSec(inputUser));
+
+        }
+        
+        static int MinInSec(int WholeNum)
+        {
+
+            int TotalInSec =  WholeNum * 60 ;
+            return TotalInSec;
+
+        }
+    }
+}
