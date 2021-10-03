4cfc3a161ebf0d6fcbbe7b41d92b3bda41162a34
AleWaymelAlexandre <103083020@student.swin.edu.au>
2021-10-03 12:44:45 +1100

    reload

--- basicfolder/basic/Program.cs
+++ basicfolder/basic/Program.cs
@@ -6,8 +6,41 @@ namespace basic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            // hello
+            // Write a console application that prints the integers from 1 to 100 (inclusive).
+
+            // But:
+
+            // for multiples of three, print Fizz (instead of the number)
+
+            // for multiples of five, print Buzz (instead of the number)
+
+            // for multiples of both three and five, print FizzBuzz (instead of the number)
+
+ 
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 5 ==0 && i % 3 == 0)
+                {
+                    System.Console.WriteLine("FizzBuzz");  
+                    continue; 
+                }
+                if ((i % 3) == 0 )
+                {
+                    System.Console.WriteLine("Fizz");
+                }
+                if ((i % 5) == 0 )
+                {
+                    System.Console.WriteLine("Buzz");   
+                }
+                
+
+                else
+                {
+                    System.Console.WriteLine(i);    
+                }
+                
+            }
+
         }
     }
 }
