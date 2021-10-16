4aa1848f82620a8813fd88128278d85c75ead4c4
AleWaymelAlexandre <103083020@student.swin.edu.au>
2021-10-16 11:53:30 +1100

    2 kata done kyu 8

---
+++ CodewarChallenge/kyu1/My_head_is_at-the_wron-end!/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace My_head_is_at_the_wron_end_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+            Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). 
+            It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+            Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+            Simples!
+
+            */
+            string[] test = {"5", "1", "3"};
+            System.Console.WriteLine("Array befor: ");
+            for (var i = 0; i < test.Length ; i++)
+            {
+                System.Console.WriteLine(test[i]);
+            }
+
+            Array.Reverse(test);  // reverse the array 
+
+            System.Console.WriteLine("\nArray After: ");
+            for (var i = 0; i < test.Length ; i++)
+            {
+                System.Console.WriteLine(test[i]);
+            }
+
+
+        }
+        // public static string[] FixTheMeerkat(string[] arr)
+        //     =>  arr.Reverse().ToArray();
+            
+    }
+}
---
+++ CodewarChallenge/kyu1/Remove_Frist_and_Last_Caracter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Remove_Frist_and_Last_Caracter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            System.Console.WriteLine( "pls entre the world you need to Remove First and Last Character {0} : ");
+            System.Console.WriteLine(Remove_char(Console.ReadLine()));
+        }
+        public static string Remove_char(string s)
+        {
+            
+            s = s.Substring(1, s.Length-2);
+            return s;
+        }   
+    }
+}
