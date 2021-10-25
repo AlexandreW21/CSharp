e0bf4986f2ad0dca3da8803d89eeac9c74598563
AleWaymelAlexandre <axl.witw@gmail.com>
2021-10-25 13:05:12 +1100

    Kyu8 reversed sequence

---
+++ CodewarChallenge/kyu8/Reversed_sequence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic; 
+
+namespace Reversed_sequence
+{
+    /*Build a function that returns an array of integers from n to 1 where n>0.
+
+        Example : n=5 --> [5,4,3,2,1]
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            foreach (var item in ReverseSeq(5))
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+        public static int[] ReverseSeq(int n)
+        {
+            int[] NArr = new int[n];
+            
+            for (int i = 0; i < n ; i++)
+                {
+                    NArr[i] = NArr.Length - i;
+                        
+                }
+            return NArr;
+        }
+        
+
+    }
+}
